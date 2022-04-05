a625f7504cd262b6e3b9098e9723e758dbbf7807
unknown <lazaro.sobral@facape.br>
2022-04-05 20:18:13 -0300

    refactor: for receive a HttpRequest

--- src/use-cases/video/create-video.ts
+++ src/use-cases/video/create-video.ts
@@ -4,14 +4,25 @@ import { Video } from '../../domain/entities/video/Video';
 import { UseCase } from '@/use-cases/ports/use-case';
 import { VideoRepository } from '@/repositories/ports/video-repository';
 import { VideoDTO } from '@/domain/entities/video/video-dto';
+import { HttpRequest } from '@/presentation/controllers/ports/http-request';
+import { SlugGenerateService } from '@/external/services';
 
 export class CreateVideo implements UseCase
 {
     constructor(private videoRepository: VideoRepository){}
 
-    async execute(request: VideoDTO): Promise<Either<InvalidSlugError, VideoDTO>> 
+    async execute(request: HttpRequest): Promise<Either<InvalidSlugError, VideoDTO>> 
     {
-        const videoOrError = Video.create(request)
+        const videoDTO: VideoDTO = {
+            url: request.body.url,
+            title: request.body.title,
+            filename: request.body.filename,
+            desc: request.body?.desc,
+            screenplay: request.body?.screenplay,
+            slug: SlugGenerateService.convertStringToSlug(request.body.slug)
+        }
+
+        const videoOrError = Video.create(videoDTO)
         if (videoOrError.isLeft()) {
           return left(videoOrError.value)
         }
