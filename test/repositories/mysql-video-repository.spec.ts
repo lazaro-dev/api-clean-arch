2a3c8ede84d1b66fb8e2947774d623862d76a53c
unknown <lazaro.sobral@facape.br>
2022-04-04 21:42:53 -0300

    test: video repository

--- test/repositories/mysql-video-repository.spec.ts
+++ test/repositories/mysql-video-repository.spec.ts
@@ -13,6 +13,15 @@ const validVideoCreateDTO: VideoDTO = {
     slug: 'titulo'
 }
 
+const validVideoCreateDTO1: VideoDTO = {
+    url: 'https://github.com',
+    title: 'Titulo 2',
+    filename: 'filename1.mp4',
+    desc: 'Descricao 2',
+    screenplay: 'Qualquer coisa',
+    slug: 'titulo-1'
+}
+
 describe('Create video use case', () => {
     let prisma: PrismaClient;
 
@@ -44,4 +53,50 @@ describe('Create video use case', () => {
         expect(videoRaw.title).toEqual(videoResponse.title)
     });
 
+    test('should be able find video by id', async () => {
+        const mysqlRepository = new MysqlVideoRepository(prisma)
+        const newVideo = await prisma.video.create({
+            data: {
+                url: validVideoCreateDTO.url,
+                filename: validVideoCreateDTO.filename,
+                desc: validVideoCreateDTO.desc,
+                title: validVideoCreateDTO.title,
+                slug: validVideoCreateDTO.slug,
+                screenplay: validVideoCreateDTO.screenplay,
+            },
+        });
+        const videoResponse: VideoData = await mysqlRepository.findById(newVideo.id);
+
+        expect(newVideo.id).toEqual(videoResponse.id)
+        expect(newVideo.slug).toEqual(videoResponse.slug)
+    });
+
+    test('should be able return all videos', async () => {
+        const mysqlRepository = new MysqlVideoRepository(prisma)
+        const newVideo = await prisma.video.create({
+            data: {
+                url: validVideoCreateDTO.url,
+                filename: validVideoCreateDTO.filename,
+                desc: validVideoCreateDTO.desc,
+                title: validVideoCreateDTO.title,
+                slug: validVideoCreateDTO.slug,
+                screenplay: validVideoCreateDTO.screenplay,
+            },
+        });
+        const newVideo1 = await prisma.video.create({
+            data: {
+                url: validVideoCreateDTO1.url,
+                filename: validVideoCreateDTO1.filename,
+                desc: validVideoCreateDTO1.desc,
+                title: validVideoCreateDTO1.title,
+                slug: validVideoCreateDTO1.slug,
+                screenplay: validVideoCreateDTO1.screenplay,
+            },
+        });
+        const videoResponse: VideoData[] = await mysqlRepository.getAll();
+
+        expect(videoResponse.length).toEqual(2)
+        // expect(videoResponse[0].id).toContain(newVideo.id)
+        // expect(videoResponse[1].id).toEqual(newVideo1.id)
+    });
 });
